b60e6c96ba01c90c9cd90a803914eadbf26289cc
Thibault-Monnier <thibaultmonni@gmail.com>
2025-09-24 14:53:10 +0200

    Allow for mutiple motors in IndependentMotorTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/IndependentMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/IndependentMotorTest.java
@@ -8,22 +8,28 @@ import org.firstinspires.ftc.teamcode.robot.Constants;
 
 @TeleOp(name = Constants.TEST_MODES_GROUP + ": Independent motor test", group = Constants.TEST_MODES_GROUP)
 public class IndependentMotorTest extends OpMode {
-    private final static double TEST_POWER = 1.0;
+    private DcMotor testMotor1;
+    private DcMotor testMotor2;
 
-    private DcMotor testMotor;
+    private void resetMotor(DcMotor motor) {
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 
     @Override
     public void init() {
-        testMotor = hardwareMap.get(DcMotor.class, Constants.TEST_MOTOR_ID);
-        testMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        testMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        testMotor1 = hardwareMap.get(DcMotor.class, Constants.TEST_MOTOR_1_ID);
+        testMotor2 = hardwareMap.get(DcMotor.class, Constants.TEST_MOTOR_2_ID);
+        resetMotor(testMotor1);
+        resetMotor(testMotor2);
 
-        telemetry.addData("Power", TEST_POWER);
+        telemetry.addData("Motor power", Constants.TEST_POWER);
         telemetry.update();
     }
 
     @Override
     public void loop() {
-        testMotor.setPower(TEST_POWER);
+        testMotor1.setPower(Constants.TEST_POWER);
+        testMotor2.setPower(Constants.TEST_POWER);
     }
 }
