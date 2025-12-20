d2c1feb9ff5b2222be586a489735d49b10a4b116
Alexandru Popescu <alexandru@popescu.is>
2025-12-20 17:59:42 +0100

    Updated MeepMeep

--- MeepMeep/src/main/java/com/acmerobotics/meepmeep/MeepMeepTesting.java
+++ MeepMeep/src/main/java/com/acmerobotics/meepmeep/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.acmerobotics.meepmeep;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -16,18 +17,36 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -15, Math.toRadians(180)))
+                .splineTo(new Vector2d(35, -30), Math.toRadians(270))
+                .strafeTo(new Vector2d(35, -40))
+                .strafeTo(new Vector2d(35, -30))
+                .splineTo(new Vector2d(-25, -25), Math.toRadians(45))
+                // Start flywheel
+                        .waitSeconds(2)
+                // Shoot artifacts
+                        .waitSeconds(2)
+                // Go back
+                .splineTo(new Vector2d(12, -30), Math.toRadians(270))
+                .strafeTo(new Vector2d(12, -40))
+                .strafeTo(new Vector2d(12, -30))
+                .splineTo(new Vector2d(-25, -25), Math.toRadians(45))
+                // Start flywheel
+                .waitSeconds(2)
+                // Shoot artifacts
+                .waitSeconds(2)
+                // Go back
+                .splineTo(new Vector2d(-11, -30), Math.toRadians(270))
+                .strafeTo(new Vector2d(-11, -40))
+                .strafeTo(new Vector2d(-11, -30))
+                .splineTo(new Vector2d(-25, -25), Math.toRadians(45))
+                // Start flywheel
+                .waitSeconds(2)
+                // Shoot artifacts
+                .waitSeconds(2)
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
