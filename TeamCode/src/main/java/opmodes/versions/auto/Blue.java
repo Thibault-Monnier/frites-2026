dd017140be67b3198190414e6be52506ef74b4b4
Thibault-Monnier <thibaultmonni@gmail.com>
2026-01-11 16:29:13 +0100

    Remove AutoOpModeOld

--- TeamCode/src/main/java/opmodes/versions/auto/Blue.java
+++ TeamCode/src/main/java/opmodes/versions/auto/Blue.java
@@ -2,17 +2,16 @@ package opmodes.versions.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import logic.RobotPosition;
 import logic.Team;
 
-import opmodes.AutoOpModeOld;
+import opmodes.AutoOpMode;
 import opmodes.GroupConstants;
 
 @Autonomous(
         name = GroupConstants.MAIN_MODES_GROUP + ": Normal Auto - BLUE Team",
         group = GroupConstants.MAIN_MODES_GROUP)
-public class Blue extends AutoOpModeOld {
+public class Blue extends AutoOpMode {
     public Blue() {
-        super(Team.BLUE, RobotPosition.StartPosition.NORMAL);
+        super(Team.BLUE);
     }
 }
--- TeamCode/src/main/java/opmodes/versions/auto/Red.java
+++ TeamCode/src/main/java/opmodes/versions/auto/Red.java
@@ -2,17 +2,16 @@ package opmodes.versions.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import logic.RobotPosition;
 import logic.Team;
 
-import opmodes.AutoOpModeOld;
+import opmodes.AutoOpMode;
 import opmodes.GroupConstants;
 
 @Autonomous(
         name = GroupConstants.MAIN_MODES_GROUP + ": Normal Auto - RED Team",
         group = GroupConstants.MAIN_MODES_GROUP)
-public class Red extends AutoOpModeOld {
+public class Red extends AutoOpMode {
     public Red() {
-        super(Team.RED, RobotPosition.StartPosition.NORMAL);
+        super(Team.RED);
     }
 }
