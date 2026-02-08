90eb0f2ea953acbdc89fc08eb925a8d0e4d18eae
Thibault-Monnier <thibaultmonni@gmail.com>
2026-02-08 18:19:56 +0100

    Allow PIDFControllerMotor to break

--- TeamCode/src/main/java/logic/pidf/PIDFControllerMotor.java
+++ TeamCode/src/main/java/logic/pidf/PIDFControllerMotor.java
@@ -26,7 +26,8 @@ public class PIDFControllerMotor extends PIDFController {
 
     /// Calculates the PID output for the saved error. The output is normalized to \[-1, 1\].
     public double get(double targetVelocity, boolean debugInfo) {
-        if (Math.abs(targetVelocity) < maxMotorVelocity / 100.0) {
+        if (Math.abs(targetVelocity) < maxMotorVelocity / 100.0
+                && Math.abs(error) < maxMotorVelocity / 100.0) {
             return 0.0;
         }
 
