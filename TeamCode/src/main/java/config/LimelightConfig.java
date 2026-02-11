d5c0dec56facdc4f40c4b529a06c6f16e9c3db56
Thibault-Monnier <thibaultmonni@gmail.com>
2026-02-11 18:19:05 +0100

    Increase Limelight stability threshold

--- TeamCode/src/main/java/config/LimelightConfig.java
+++ TeamCode/src/main/java/config/LimelightConfig.java
@@ -6,6 +6,6 @@ import com.bylazar.configurables.annotations.Configurable;
 @Config
 @Configurable
 public class LimelightConfig {
-    public static double STABILITY_THRESHOLD_METERS = 0.08;
+    public static double STABILITY_THRESHOLD_METERS = 0.15;
     public static int FRAMES_IN_A_ROW_THRESHOLD = 4;
 }
