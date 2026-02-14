cc9e5896f95b3121e0e99b5d7e22f88e2c750f47
Thibault-Monnier <thibaultmonni@gmail.com>
2026-02-14 20:58:42 +0100

    Lower KalmanFilter model variance

--- TeamCode/src/main/java/config/KalmanFilterConfig.java
+++ TeamCode/src/main/java/config/KalmanFilterConfig.java
@@ -9,8 +9,8 @@ import math.Distance;
 @Config
 @Configurable
 public class KalmanFilterConfig {
-    public static Distance MODEL_VARIANCE_DIST = Distance.fromMillimeters(5);
-    public static Angle MODEL_VARIANCE_ANGLE = Angle.fromDegrees(2);
+    public static Distance MODEL_VARIANCE_DIST = Distance.fromMillimeters(1);
+    public static Angle MODEL_VARIANCE_ANGLE = Angle.fromDegrees(0.5);
 
     public static Distance CAMERA_VARIANCE_DIST = Distance.fromMillimeters(30);
     public static Angle CAMERA_VARIANCE_ANGLE = Angle.fromDegrees(5);
