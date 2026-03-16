5a2e7b4148d90035ba09fbc7beb8e60a67f6b860
Thibault-Monnier <thibaultmonni@gmail.com>
2026-03-16 21:29:06 +0100

    Adjust cannon config

--- TeamCode/src/main/java/config/CannonConfig.java
+++ TeamCode/src/main/java/config/CannonConfig.java
@@ -13,7 +13,7 @@ import math.Vector2D;
 @Configurable
 public class CannonConfig {
     public static double MOVING_SPEED = 1.0;
-    public static double REVERSE_SPEED = -0.65;
+    public static double REVERSE_SPEED = -1.0;
 
     public static PIDFCoefficients CANNON_PID = new PIDFCoefficients(0.02, 0.0, 0.0, -0.5);
 
@@ -22,11 +22,11 @@ public class CannonConfig {
     public static Vector2D CANNON_RELATIVE_POSITION =
             new Vector2D(Distance.fromCentimeters(-15.0), Distance.fromCentimeters(0.0));
 
-    public static double SHOOT_DELAY = 0.375;
-    public static int SHOOT_BALLS_AMOUNT = 4;
+    public static double SHOOT_DELAY = 0.4;
+    public static int SHOOT_BALLS_AMOUNT = 3;
 
     public static double CALIBRATION_SPEED_CHANGE_OFFSET = 25.0;
 
-    public static double ERROR_MARGIN = 40.0;
-    public static double STABLE_THRESHOLD = 30.0;
+    public static double ERROR_MARGIN = 25.0;
+    public static double STABLE_THRESHOLD = 20.0;
 }
