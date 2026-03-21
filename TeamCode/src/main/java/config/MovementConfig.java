40bd6ec1379c25fcca0f80e7faee0616ef80b322
Thibault-Monnier <thibaultmonni@gmail.com>
2026-03-21 21:34:32 +0100

    Tweak movement config

--- TeamCode/src/main/java/config/MovementConfig.java
+++ TeamCode/src/main/java/config/MovementConfig.java
@@ -18,15 +18,15 @@ public class MovementConfig {
 
     public static double SPEED_MULTIPLIER = 1.0;
     public static double SLOW_SPEED_MULTIPLIER = 0.5;
-    public static double SUPER_SLOW_SPEED_MULTIPLIER = 0.2;
+    public static double SUPER_SLOW_SPEED_MULTIPLIER = 0.225;
 
     public static PIDFLCoefficients TURN_PIDF_COEFFICIENTS =
             new PIDFLCoefficients(1.0, 0.0, 0.05, 0.0, 0.1);
-    public static Angle TURN_TOLERANCE = Angle.fromDegrees(4.0);
+    public static Angle TURN_TOLERANCE = Angle.fromDegrees(5.0);
     public static Angle NOT_TURNING_THRESHOLD = Angle.fromDegrees(0.5); // per frame
 
     public static PIDFLCoefficients TRANSLATION_PIDF_COEFFICIENTS =
-            new PIDFLCoefficients(0.003, 0, 0.00004, 0.025, 0.0);
-    public static Distance TRANSLATION_TOLERANCE = Distance.fromCentimeters(5);
-    public static Distance NOT_TRANSLATING_THRESHOLD = Distance.fromCentimeters(0.1); // per frame
+            new PIDFLCoefficients(0.0025, 0, 0.00045, 0.0, 0.1);
+    public static Distance TRANSLATION_TOLERANCE = Distance.fromMillimeters(5);
+    public static Distance NOT_TRANSLATING_THRESHOLD = Distance.fromMillimeters(1); // per frame
 }
