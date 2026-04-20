5eb86c9ba52f3b5b510ca607a9cba76c2c2d5cd2
Thibault-Monnier <thibaultmonni@gmail.com>
2026-04-20 16:42:32 +0200

    Update odometry pod offsets

--- TeamCode/src/main/java/config/OdometryConfig.java
+++ TeamCode/src/main/java/config/OdometryConfig.java
@@ -10,8 +10,8 @@ import utils.math.Distance;
 @Config
 @Configurable
 public class OdometryConfig {
-    public static Distance ENCODER_X_Y_OFFSET = Distance.fromMillimeters(94);
-    public static Distance ENCODER_Y_X_OFFSET = Distance.fromMillimeters(-155);
+    public static Distance ENCODER_X_Y_OFFSET = Distance.fromMillimeters(92.5);
+    public static Distance ENCODER_Y_X_OFFSET = Distance.fromMillimeters(-155.678);
 
     public static GoBildaPinpointDriver.EncoderDirection ENCODER_X_DIRECTION =
             GoBildaPinpointDriver.EncoderDirection.REVERSED;
